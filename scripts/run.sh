e7f11de0961f131f54e27bbef8351084c678a787
胡月月 <huyueyue01@bilibili.com>
2023-10-30 18:17:58 +0800

    set roop path

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,9 @@ export BASICSR_JIT=True
 conf_name='HQ_Dictionary'
 # conf_name='RestoreFormer'
 
-ROOT_PATH='' # The path for saving model and logs
+ROOT_PATH='results/models' # The path for saving model and logs
 
-gpus='0,1,2,3'
+gpus='0'
 
 #P: pretrain SL: soft learning
 node_n=1
